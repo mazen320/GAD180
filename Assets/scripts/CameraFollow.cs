f186199861f47686cbc88e5d2f7c310d8b8370cb
mazen320 <66998929+mazen320@users.noreply.github.com>
2020-11-03 23:21:59 +0400

    tiny performance fixes(i think), just pushing latest side on mine

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -9,10 +9,7 @@ public class CameraFollow : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
      
     void Update()
-         {
-             // Define a target position above and behind the target transform
-             Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -5));
-     
+         {                
              transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
          }
 }
