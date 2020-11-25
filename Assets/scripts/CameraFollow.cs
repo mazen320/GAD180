398c19b062cf036356f9a09f3ee8646559cfaefc
Mazen <mazenshaar@hotmail.com>
2020-11-25 12:28:47 +0400

    Fixed camera positioning, and the second tank

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -10,7 +10,7 @@ public class CameraFollow : MonoBehaviour
      
     void Update()
          {                
-            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -5));
+            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
          }
 }
