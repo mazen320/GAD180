2086d02d2782b717b75861b2772520a729c107a9
Mazen <mazenshaar@hotmail.com>
2020-11-29 20:50:09 +0400

    Me and Mohamed came up with solutions for the turning and changed the UI together following some minor tweaks for QOL

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -7,9 +7,13 @@ public class Movement : MonoBehaviour
     public float movementSpeed = 5.0f;
     public float rotationSpeed = 30.0f;
 
+    public float torque = 5.0f;
+    private Rigidbody rb;
+
     
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,7 +22,8 @@ public class Movement : MonoBehaviour
         float turn = Input.GetAxis("Horizontal");
         float forwards = Input.GetAxis("Vertical");
 
-        transform.Rotate(0, 0, turn *rotationSpeed*Time.deltaTime);
+        transform.Rotate(0, turn * rotationSpeed,0);
+        //rb.AddTorque(transform.up * torque * turn);
         transform.Translate(0, 0, forwards * Time.deltaTime * movementSpeed);
     }
     void FixedUpdate() 
