cb0892a299741954e87edb757127c3a21d1451c3
Mazen <mazenshaar@hotmail.com>
2020-12-05 23:18:53 +0400

    Added spawnable point objects in the map, at random coordinates, and also added the point system to work on collision, and some fixes.

---
+++ Assets/scripts/PointSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour
+{
+    public int points = 0;
+    private string Points;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col) 
+    {
+        if (col.gameObject.tag == "pointobj")
+        {
+            Debug.Log(points);
+            col.gameObject.SetActive(false);
+            points ++;
+        }
+    }
+}
